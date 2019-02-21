863ce3eaf9983be1a80d65c0fb1d99aabea1739d
sathi2 <sathi@wired2perform.com>
2019-02-21 21:16:46 +0530

    New service. jx import shoild run - 199e54a5f4137f8a4a8eff9658dfffe62f8158c6

---
+++ src/main/java/com/example/springservice8/SpringService8Application.java
@@ -0,0 +1,13 @@
+package com.example.springservice8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringService8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringService8Application.class, args);
+	}
+
+}
